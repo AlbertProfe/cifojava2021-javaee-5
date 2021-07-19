15255b9552d511f804d1de48c923cd635da57cb8
admin CIFO <admin CIFO@DESKTOP-8S89R17>
2021-07-19 13:57:36 +0200

    update course status

--- employeeCourse2/src/main/java/dev/example/employeeCourse/boot/controller/EmployeeController.java
+++ employeeCourse2/src/main/java/dev/example/employeeCourse/boot/controller/EmployeeController.java
@@ -1,7 +1,5 @@
 package dev.example.employeeCourse.boot.controller;
 
-
-
 import java.io.IOException;
 import java.util.Optional;
 
@@ -21,12 +19,13 @@ import dev.example.employeeCourse.boot.model.Course;
 import dev.example.employeeCourse.boot.model.Employee;
 import dev.example.employeeCourse.boot.model.EmployeeImage;
 import dev.example.employeeCourse.boot.model.Enrollment;
+import dev.example.employeeCourse.boot.model.Expense;
 import dev.example.employeeCourse.boot.repository.CertificateRepository;
 import dev.example.employeeCourse.boot.repository.CourseRepository;
 import dev.example.employeeCourse.boot.repository.EmployeeImageRepository;
 import dev.example.employeeCourse.boot.repository.EmployeeRepository;
 import dev.example.employeeCourse.boot.repository.EnrollmentRepository;
-
+import dev.example.employeeCourse.boot.repository.ExpenseRepository;
 
 @Controller
 @RequestMapping("/employee")
@@ -34,29 +33,31 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeRepository employeeRepository;
-	
+
 	@Autowired
 	CourseRepository courseRepository;
-	
+
 	@Autowired
 	CertificateRepository certificateRepository;
-	
+
 	@Autowired
 	EnrollmentRepository enrollmentRepository;
-	
+
 	@Autowired
 	EmployeeImageRepository employeeImageRepository;
+	
+	@Autowired
+	ExpenseRepository expenseRepository;
 
 	@RequestMapping("/allEmployees")
 	public String getAllEmployees(Model boxToView) {
 
-		boxToView.addAttribute("employeeListfromControllerAndDB", 
-				employeeRepository.findAll());
+		boxToView.addAttribute("employeeListfromControllerAndDB", employeeRepository.findAll());
 
 		return "employee/employees";
 	}
 
-	//-----------------------delete----------------------------------
+	// -----------------------delete----------------------------------
 	@RequestMapping("/deleteEmployee")
 	public String removeEmployee(int id, Model model) {
 
@@ -81,17 +82,15 @@ public class EmployeeController {
 	}
 
 	@RequestMapping("/deleteAllEmployees")
-	public String deleteAllEmployees () {
+	public String deleteAllEmployees() {
 
-		
-		 employeeRepository.deleteAll();
-		
+		employeeRepository.deleteAll();
 
 		return "redirect:/employee/allEmployees";
 
 	}
-	
-	//-----------------------add----------------------------------
+
+	// -----------------------add----------------------------------
 	@RequestMapping("/newEmployee")
 	public String newEmpoyee() {
 
@@ -106,7 +105,7 @@ public class EmployeeController {
 		return "redirect:/employee/allEmployees";
 	}
 
-	//-----------------------update----------------------------------
+	// -----------------------update----------------------------------
 	@RequestMapping("/updateEmployee")
 	public String updateEmpoyee(int id, Model model) {
 
@@ -149,147 +148,200 @@ public class EmployeeController {
 			return "home/notfound.html";
 
 	}
-	
-	//-----------------------detail----------------------------------
+
+	// -----------------------detail----------------------------------
 	@RequestMapping("/detailEmployee")
 	public String detailEmployee(int id, Model model) {
-		
 
 		Optional<Employee> employeeFound = findOneEmployeeById(id);
 
 		if (employeeFound.isPresent()) {
 
 			model.addAttribute("employeefromController", employeeFound.get());
-			
-			 Optional<Iterable<Enrollment>> enrollmentFound =
-			  enrollmentRepository.findAllEnrollmentByEmployee(
-					  employeeRepository.findById(id).get());
-			  
-				  model.addAttribute("enrollmentfromController", enrollmentFound.get());
-				 
-			 
-			
+
+			Optional<Iterable<Enrollment>> enrollmentFound = enrollmentRepository
+					.findAllEnrollmentByEmployee(employeeRepository.findById(id).get());
+
+			model.addAttribute("enrollmentfromController", enrollmentFound.get());
+
+			Optional<Iterable<Expense>> expenseFound = expenseRepository
+					.findAllExpenseByEmployee(employeeRepository.findById(id).get());
+
+			model.addAttribute("expensefromController", expenseFound.get());
+
 			return "employee/detailemployee";
 		}
 
 		else
 			return "home/notfound.html";
 	}
-	
-	//----------------------- enrollment ----------------------------------
-		@RequestMapping("/addCourse")
-		public String addCourseEmployee(int id, Model model) {
 
-			Optional<Employee> employeeFound = findOneEmployeeById(id);
+	// ----------------------- enrollment ----------------------------------
+	@RequestMapping("/addCourse")
+	public String addCourseEmployee(int id, Model model) {
 
-			if (employeeFound.isPresent()) {
+		Optional<Employee> employeeFound = findOneEmployeeById(id);
 
-				model.addAttribute("employeefromController", employeeFound.get());
-				model.addAttribute("coursesfromController", courseRepository.findAll());
-				model.addAttribute("certificatesfromController", certificateRepository.findAll());
-				
-				return "employee/addcourseemployee";
-			}
+		if (employeeFound.isPresent()) {
 
-			else
-				return "home/notfound.html";
+			model.addAttribute("employeefromController", employeeFound.get());
+			model.addAttribute("coursesfromController", courseRepository.findAll());
+			model.addAttribute("certificatesfromController", certificateRepository.findAll());
+
+			return "employee/addcourseemployee";
 		}
-		
-		
-		@RequestMapping(value = "/insertCourse", method = RequestMethod.POST)
-		public String insertCourseEmployee(int id,Employee employee, 
-				 @RequestParam("idCourse") int idCourse,  RedirectAttributes redirectAttributes) {
-			
-			System.out.println(" (1) id course: " + idCourse + " id employee: " + id);
 
-			Optional<Employee> employeeFound = findOneEmployeeById(id);
-			Optional<Course> courseFound = findOneCourseById(idCourse);
+		else
+			return "home/notfound.html";
+	}
 
-			if (employeeFound.isPresent() && courseFound.isPresent()) {
-				System.out.println(" (2) id course: " + idCourse + " id employee: " + id);
-				Enrollment enrollment = new Enrollment();
-				
-				enrollment.setCourse(courseFound.get());
-				enrollment.setEmployee(employeeFound.get());
-				
-				enrollmentRepository.save(enrollment);
-				
-				redirectAttributes.addFlashAttribute("message",
-						"You successfully enrolled " + employeeFound.get().getName() +" "+  employeeFound.get().getSurname() + " in "+
-								courseFound.get().getCertificate().getName() + "!");
-				
-				return "redirect:/employee/addCourse?id=" + id;
-			}
+	@RequestMapping(value = "/insertCourse", method = RequestMethod.POST)
+	public String insertCourseEmployee(int id, Employee employee, @RequestParam("idCourse") int idCourse,
+			RedirectAttributes redirectAttributes) {
 
-			else
-				return "home/notfound.html";
-		}
-		
-		//----------------------- add image employee ---------------------------------	
-		@RequestMapping("/addImageEmployee")
-		public String addImageEmployee(int id, Model model) {
+		System.out.println(" (1) id course: " + idCourse + " id employee: " + id);
 
-			Optional<Employee> employeeFound = findOneEmployeeById(id);
+		Optional<Employee> employeeFound = findOneEmployeeById(id);
+		Optional<Course> courseFound = findOneCourseById(idCourse);
 
-			if (employeeFound.isPresent()) {
+		if (employeeFound.isPresent() && courseFound.isPresent()) {
+			System.out.println(" (2) id course: " + idCourse + " id employee: " + id);
+			Enrollment enrollment = new Enrollment();
 
-				model.addAttribute("employeefromController", employeeFound.get());
-				
-				return "employee/addimage";
-			}
+			enrollment.setCourse(courseFound.get());
+			enrollment.setEmployee(employeeFound.get());
 
-			else
-				return "home/notfound.html";
-		}
-		
-		@PostMapping("/insertEmployeeImage")
-		public String insertEmployeeImage(@RequestParam String name, @RequestParam int employeeId,  @RequestParam MultipartFile file, RedirectAttributes redirectAttributes)
-				throws IOException {
-			
-			//System.out.println("Image name: " + name);
-
-			EmployeeImage employeeImage = new EmployeeImage();
-			employeeImage.setName(name);
-			employeeImage.setEmployeeId(employeeId);
-			employeeImage.setImage(new Binary(file.getBytes()));
-			
-			employeeImageRepository.save(employeeImage);
-			
-			Optional<Employee> employeeFound = findOneEmployeeById(employeeId);
-
-			if (employeeFound.isPresent()) {
-				
-				employeeFound.get().setEmployeeImageId(employeeImage.getId());
-				employeeRepository.save(employeeFound.get());
-			}
-			
-			
+			enrollmentRepository.save(enrollment);
 
 			redirectAttributes.addFlashAttribute("message",
-					"You successfully uploaded " + file.getOriginalFilename() + "!");
+					"You successfully enrolled " + employeeFound.get().getName() + " "
+							+ employeeFound.get().getSurname() + " in " + courseFound.get().getCertificate().getName()
+							+ "!");
 
-			return "redirect:/employee/addImageEmployee?id=" + employeeId;
+			return "redirect:/employee/addCourse?id=" + id;
 		}
-		
+
+		else
+			return "home/notfound.html";
+	}
 	
+	@RequestMapping("/updateEmployeeCourse")
+	public String updateCourseEmployee(int id, Model model) {
+
+		Optional<Enrollment> enrollmentFound = findOneEnrollmentById(id);
+
+		if (enrollmentFound.isPresent()) {
+			
+		
+			model.addAttribute("employeefromController", enrollmentFound.get().getEmployee());
+			model.addAttribute("enrollment", enrollmentFound.get());
+			
+
+			return "employee/updatecourseemployee";
+		}
+
+		else
+			return "home/notfound.html";
+		
+
+	}
+	
+	
+	@PostMapping("/replaceEmployeeCourse/{idEnrollment}")
+	public String replaceCourseEmployee(@PathVariable("idEnrollment") int id, Enrollment enrollment, RedirectAttributes redirectAttributes) {
+
+		System.out.println( id);
+		
+		Optional<Enrollment> enrollmentFound = findOneEnrollmentById(id);
+
+		if (enrollmentFound.isPresent()) {
+
+			enrollmentFound.get().setStatus(enrollment.getStatus());
+			enrollmentFound.get().setApproved(enrollment.isApproved());
+
+			enrollmentRepository.save(enrollmentFound.get());
+			
+			redirectAttributes.addFlashAttribute("message",
+					"You successfully updated that enrollment !");
+
+			return "redirect:/employee/updateEmployeeCourse?id=" + id;
+			
+
+		} else
+			return "home/notfound.html";
+
+	}
+
+	// ----------------------- add image employee ---------------------------------
+	@RequestMapping("/addImageEmployee")
+	public String addImageEmployee(int id, Model model) {
+
+		Optional<Employee> employeeFound = findOneEmployeeById(id);
+
+		if (employeeFound.isPresent()) {
+
+			model.addAttribute("employeefromController", employeeFound.get());
+
+			return "employee/addimage";
+		}
+
+		else
+			return "home/notfound.html";
+	}
+
+	@PostMapping("/insertEmployeeImage")
+	public String insertEmployeeImage(@RequestParam String name, @RequestParam int employeeId,
+			@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+
+		// System.out.println("Image name: " + name);
+
+		EmployeeImage employeeImage = new EmployeeImage();
+		employeeImage.setName(name);
+		employeeImage.setEmployeeId(employeeId);
+		employeeImage.setImage(new Binary(file.getBytes()));
+
+		employeeImageRepository.save(employeeImage);
+
+		Optional<Employee> employeeFound = findOneEmployeeById(employeeId);
+
+		if (employeeFound.isPresent()) {
+
+			employeeFound.get().setEmployeeImageId(employeeImage.getId());
+			employeeRepository.save(employeeFound.get());
+		}
+
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/employee/addImageEmployee?id=" + employeeId;
+	}
+	
+	
+	
+
 //--------------------------------------------------------------------------------
 //------------------------- service to controller --------------------------------
 //--------------------------------------------------------------------------------
 
 	public Optional<Employee> findOneEmployeeById(int id) {
 
-		
 		Optional<Employee> employeeFound = employeeRepository.findById(id);
-		
+
 		return employeeFound;
 	}
-	
+
 	public Optional<Course> findOneCourseById(int id) {
 
-		
 		Optional<Course> courseFound = courseRepository.findById(id);
-		
+
 		return courseFound;
+	}
+	
+	public Optional<Enrollment> findOneEnrollmentById(int id) {
+
+		Optional<Enrollment> enrollmentFound = enrollmentRepository.findById(id);
+
+		return enrollmentFound;
 	}
 
 }
--- employeeCourse2/src/main/java/dev/example/employeeCourse/boot/repository/ExpenseRepository.java
+++ employeeCourse2/src/main/java/dev/example/employeeCourse/boot/repository/ExpenseRepository.java
@@ -1,9 +1,14 @@
 package dev.example.employeeCourse.boot.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
+import dev.example.employeeCourse.boot.model.Employee;
 import dev.example.employeeCourse.boot.model.Expense;
 
 public interface ExpenseRepository extends CrudRepository<Expense,Integer> {
+
+	Optional<Iterable<Expense>> findAllExpenseByEmployee(Employee employee);
 
 }
